52ae1911e8f2922aa6f13cccab096a16605fc3b3
Felipe Oliveira <felipe.ed.oliveira@gmail.com>
2025-01-14 09:13:28 -0300

    refactor: break logic in more functions and  create func to close channels

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
 	defer cancelFn()
 
 	buf := []chan int{}
+	defer clearBuffers(buf)
 
 	//creating 10 buffered channels
 	for i := 0; i < 10; i++ {
@@ -34,57 +35,12 @@ func main() {
 	// creating 10 producers
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
-		go func(ctx context.Context, producerId int, buffs []chan int, g *sync.WaitGroup) {
-			l := slog.With(slog.Int("producer", producerId))
-			for j := 0; j < 100; j++ {
-				select {
-				case <-ctx.Done():
-					return
-				default:
-					buffIndex := rand.Intn(len(buffs))
-					l.Info(fmt.Sprintf("producer:%d peaking buffer:%d", producerId, buffIndex))
-					b := buffs[buffIndex]
-
-					select {
-					case b <- rand.Intn(1000):
-						l.Info(fmt.Sprintf("producer: %d pushed to buffer: %d", producerId, buffIndex))
-						time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-						break
-					default:
-						l.Warn(fmt.Sprintf("producer: %d buffer: %d is full. new attempt in 1 second", producerId, buffIndex))
-			
-						time.Sleep(time.Second)
-					}
-				}
-			}
-			g.Done()
-		}(ctx, i, buf, &wg)
+		go produce(ctx, i, buf, &wg)
 	}
 
 	//creating 10 consumers
 	for i := 0; i < 10; i++ {
-		go func(ctx context.Context, consumerId int, buffs []chan int) {
-			l := slog.With(slog.Int("consumer", consumerId))
-			for {
-				select {
-				case <-ctx.Done():
-					return
-				default:
-					buffIndex := rand.Intn(len(buffs))
-					b := buffs[buffIndex]
-
-					l.Info(fmt.Sprintf("consumer:%d peaked buffer:%d", consumerId, buffIndex), slog.Int("buffer", buffIndex), slog.Int("cap", cap(b)), slog.Int("len", len(b)))
-
-					if len(b) == 0 {
-						l.Warn(fmt.Sprintf("consumer:%d buffer:%d is empty. try new buffer", consumerId, buffIndex), slog.Int("cap", cap(b)), slog.Int("len", len(b)))
-						time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-						continue
-					}
-					l.Info(fmt.Sprintf("consumer:%d popped from buffer:%d value:%d", consumerId, buffIndex, <-b), slog.Int("buffer", buffIndex), slog.Int("cap", cap(b)), slog.Int("len", len(b)))
-					time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-				}
-			}
-		}(ctx, i, buf)
+		go consume(ctx, i, buf)
 	}
 
 	quit := make(chan os.Signal, 1)
@@ -100,7 +56,6 @@ func main() {
 		select {
 		case <-quit:
 			slog.Info("received interrupt signal. stopping program")
-			cancelFn()
 		case <-ctx.Done():
 			slog.Info("application timeout. stopping program")
 			return
@@ -109,5 +64,59 @@ func main() {
 			return
 		}
 	}
+}
 
+func produce(ctx context.Context, producerId int, buffs []chan int, g *sync.WaitGroup) {
+	l := slog.With(slog.Int("producer", producerId))
+	for j := 0; j < 100; j++ {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			buffIndex := rand.Intn(len(buffs))
+			l.Info(fmt.Sprintf("producer:%d peaking buffer:%d", producerId, buffIndex))
+			b := buffs[buffIndex]
+
+			select {
+			case b <- rand.Intn(1000):
+				l.Info(fmt.Sprintf("producer: %d pushed to buffer: %d", producerId, buffIndex))
+				time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+				break
+			default:
+				l.Warn(fmt.Sprintf("producer: %d buffer: %d is full. new attempt in 1 second", producerId, buffIndex))
+
+				time.Sleep(time.Second)
+			}
+		}
+	}
+	g.Done()
+}
+
+func consume(ctx context.Context, consumerId int, buffs []chan int) {
+	l := slog.With(slog.Int("consumer", consumerId))
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			buffIndex := rand.Intn(len(buffs))
+			b := buffs[buffIndex]
+
+			l.Info(fmt.Sprintf("consumer:%d peaked buffer:%d", consumerId, buffIndex), slog.Int("buffer", buffIndex), slog.Int("cap", cap(b)), slog.Int("len", len(b)))
+
+			if len(b) == 0 {
+				l.Warn(fmt.Sprintf("consumer:%d buffer:%d is empty. try new buffer", consumerId, buffIndex), slog.Int("cap", cap(b)), slog.Int("len", len(b)))
+				time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+				continue
+			}
+			l.Info(fmt.Sprintf("consumer:%d popped from buffer:%d value:%d", consumerId, buffIndex, <-b), slog.Int("buffer", buffIndex), slog.Int("cap", cap(b)), slog.Int("len", len(b)))
+			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+		}
+	}
+}
+
+func clearBuffers(ch []chan int) {
+	for _, c := range ch {
+		close(c)
+	}
 }
